f3618f76f98c8fe4adedef5b8916dadeddbe1ab7
hydrz <n.haoyuan@gmail.com>
2025-09-18 23:48:04 +0800

    feat: integrate minification and remove unused sitemap and robots.txt files

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import { defineConfig } from "vite";
+import { ViteMinifyPlugin } from "vite-plugin-minify";
 import Sitemap from "vite-plugin-sitemap";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -14,11 +15,25 @@ export default defineConfig({
 			generateRobotsTxt: true,
 			robots: [{ userAgent: "*", allow: "/" }],
 		}),
+		ViteMinifyPlugin({}),
 	],
 	build: {
+		minify: "terser",
+		sourcemap: false,
+		reportCompressedSize: true,
+		terserOptions: {
+			compress: {
+				drop_console: true,
+				drop_debugger: true,
+			},
+		},
 		rollupOptions: {
 			input: {
-				main: resolve(__dirname, "index.html"),
+				index: resolve(__dirname, "index.html"),
+			},
+			output: {
+				chunkFileNames: "assets/[name]-[hash].js",
+				assetFileNames: "assets/[name]-[hash].[ext]",
 			},
 		},
 	},
